aba180bf614e40fae72d99ffb4e687d5a4dcd7a0
psingman <paulesingman@gmail.com>
2016-08-17 01:22:55 -0400

    implementation of deque ADT

---
+++ dequeue.py
@@ -0,0 +1,24 @@
+# python implementation of deque
+
+class Deque:
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def addRear(self, item):
+        self.items.insert(0, item)
+
+    def removeFront(self, item):
+        return self.items.pop()
+
+    def removeRear(self, item):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
