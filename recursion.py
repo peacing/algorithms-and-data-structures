84bd5e1cc3dc4dfdaa7440adb95beed457186a70
psingman <paulesingman@gmail.com>
2016-08-21 15:20:41 -0400

    algs and ds class classes

---
+++ recursion.py
@@ -0,0 +1,96 @@
+# recursive functions
+
+def toStr(n, base):
+    convertString = '123456789ABCDEF'
+    if n < base:
+        return convertString[n]
+    else:
+        return toStr(n // base, base) + convertString[n % base]
+
+import turtle
+
+myTurtle =  turtle.Turtle()
+myWin = turtle.Screen()
+
+def drawSpiral(myTurtle, lineLen):
+    if lineLen > 0:
+        myTurtle.forward(lineLen)
+        myTurtle.right(90)
+        drawSpiral(myTurtle, lineLen-5)
+
+
+
+def drawTriangle(points,color,myTurtle):
+    myTurtle.fillcolor(color)
+    myTurtle.up()
+    myTurtle.goto(points[0][0],points[0][1])
+    myTurtle.down()
+    myTurtle.begin_fill()
+    myTurtle.goto(points[1][0],points[1][1])
+    myTurtle.goto(points[2][0],points[2][1])
+    myTurtle.goto(points[0][0],points[0][1])
+    myTurtle.end_fill()
+
+def getMid(p1,p2):
+    return ( (p1[0]+p2[0]) / 2, (p1[1] + p2[1]) / 2)
+
+def sierpinski(points,degree,myTurtle):
+    colormap = ['blue','red','green','white','yellow',
+                'violet','orange']
+    drawTriangle(points,colormap[degree],myTurtle)
+    if degree > 0:
+        sierpinski([points[0],
+                        getMid(points[0], points[1]),
+                        getMid(points[0], points[2])],
+                   degree-1, myTurtle)
+        sierpinski([points[1],
+                        getMid(points[0], points[1]),
+                        getMid(points[1], points[2])],
+                   degree-1, myTurtle)
+        sierpinski([points[2],
+                        getMid(points[2], points[1]),
+                        getMid(points[0], points[2])],
+                   degree-1, myTurtle)
+
+def main():
+   myTurtle = turtle.Turtle()
+   myWin = turtle.Screen()
+   myPoints = [[-100,-50],[0,100],[100,-50]]
+   sierpinski(myPoints,3,myTurtle)
+   myWin.exitonclick()
+
+#main()
+
+# making change dynamically
+
+def dpMakeChange(coinValueList,change,minCoins,coinsUsed):
+   for cents in range(change+1):
+      coinCount = cents
+      newCoin = 1
+      for j in [c for c in coinValueList if c <= cents]:
+            if minCoins[cents-j] + 1 < coinCount:
+               coinCount = minCoins[cents-j]+1
+               newCoin = j
+      minCoins[cents] = coinCount
+      coinsUsed[cents] = newCoin
+   return minCoins[change]
+
+def printCoins(coinsUsed,change):
+   coin = change
+   while coin > 0:
+      thisCoin = coinsUsed[coin]
+      print(thisCoin)
+      coin = coin - thisCoin
+
+def main():
+    amnt = 63
+    clist = [1,5,10,21,25]
+    coinsUsed = [0]*(amnt+1)
+    coinCount = [0]*(amnt+1)
+
+    print("Making change for", amnt, "requires")
+    print(dpMakeChange(clist, amnt, coinCount, coinsUsed), "coins")
+    print("They are:")
+    printCoins(coinsUsed, amnt)
+    print("The used list is as follows:")
+    print(coinsUsed)
